b1a6d4f570229fd76464297d3b7d7f15861a6e84
Lauren Stevens <57653314+laurenstevens257@users.noreply.github.com>
2025-05-27 15:11:18 -0700

    Update off_to_red_clock.v

--- off_to_red_clock.v
+++ off_to_red_clock.v
@@ -1,1 +1,25 @@
+module traffic_light(
+    input wire clk,         // 100 MHz clock
+    output reg red_light,
+    output reg yellow_light,
+    output reg green_light
+);
+
+  // 500 million clock cycles = 5 seconds @ 100 MHz
+  reg [32:0] counter = 0;
+
+  always @(posedge clk) begin
+    // Default: all lights off
+    red_light <= 0;
+    yellow_light <= 0;
+    green_light <= 0;
+
+    if (counter < 500_000_000) begin
+      counter <= counter + 1;
+    end else begin
+      red_light <= 1;  // Turn red on after 5 seconds
+    end
+  end
+
+endmodule
 
